35e02f246ce31ea06a2def0eb2ac153eb55d2bf2
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-12-23 15:59:07 +0200

    refactoring

--- src/main/java/ua/shortener/link/controller/LinkRestController.java
+++ src/main/java/ua/shortener/link/controller/LinkRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import ua.shortener.link.entity.Link;
 import ua.shortener.link.service.LinkService;
 
