4d99c98e073fc7d53529b1f64199d7bb1faf90c9
Eugen2510 <Lugovoj20@gmail.com>
2023-12-28 15:16:18 +0200

    added password validation and change classes with request and response

---
+++ src/main/java/ua/shortener/security/auth/dto/login/ResponseLoginError.java
@@ -0,0 +1,15 @@
+package ua.shortener.security.auth.dto.login;
+
+public enum ResponseLoginError {
+    OK("Successfully"),
+    BAD_PASSWORD_OR_LOGIN("Wrong password or login");
+
+    private final String message;
+    ResponseLoginError(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
