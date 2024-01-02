817785383d538ff3a165e7a2bb8e562ee09c47fd
fear1611 <127629681+fear1611@users.noreply.github.com>
2024-01-02 16:15:43 +0200

    done task Add User in Link (LinkRestController)

--- src/main/java/ua/shortener/link/controller/LinkRestController.java
+++ src/main/java/ua/shortener/link/controller/LinkRestController.java
@@ -7,7 +7,13 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import ua.shortener.link.Link;
 import ua.shortener.link.dto.DTOLink;
