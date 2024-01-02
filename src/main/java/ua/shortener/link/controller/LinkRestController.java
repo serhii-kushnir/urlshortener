75b514fd6bf5e21f2367cee1d8e7d6ce307bc99a
fear1611 <127629681+fear1611@users.noreply.github.com>
2024-01-02 16:12:50 +0200

    done task Add User in Link (LinkRestController)

--- src/main/java/ua/shortener/link/controller/LinkRestController.java
+++ src/main/java/ua/shortener/link/controller/LinkRestController.java
@@ -2,16 +2,12 @@ package ua.shortener.link.controller;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.data.crossstore.ChangeSetPersister;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import ua.shortener.link.Link;
 import ua.shortener.link.dto.DTOLink;
@@ -22,6 +18,7 @@ import ua.shortener.user.service.UserRepository;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/link")
@@ -56,17 +53,25 @@ public final class LinkRestController {
 
 
     @PostMapping("/create")
-    public ResponseEntity<DTOLink> createLink(final @RequestBody DTOLink dtoLink) throws ChangeSetPersister.NotFoundException {
-        User existingUser = userRepository.findById(1L)
-                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+    public ResponseEntity<DTOLink> createLink(final @RequestBody DTOLink dtoLink) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail = authentication.getName();
 
-        Link link = new Link();
-        link.setUrl(dtoLink.getLink());
-        link.setUser(existingUser);
+        Optional<User> optionalUser = userRepository.findUserByEmail(userEmail);
 
-        linkService.createLink(link);
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
 
-        return ResponseEntity.ok(link.toDTO());
+            Link link = new Link();
+            link.setUrl(dtoLink.getLink());
+            link.setUser(existingUser);
+
+            linkService.createLink(link);
+
+            return ResponseEntity.ok(link.toDTO());
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 
@@ -87,5 +92,10 @@ public final class LinkRestController {
     public ResponseEntity<Void> deleteLink(final @PathVariable String shortLink) {
         linkService.deleteLink(shortLink);
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
