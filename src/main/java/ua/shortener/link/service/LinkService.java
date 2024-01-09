73ae703795e44ed77ce7dac2ad1bec60eaf9debd
fear1611 <127629681+fear1611@users.noreply.github.com>
2024-01-09 16:45:41 +0200

    cash

--- src/main/java/ua/shortener/link/service/LinkService.java
+++ src/main/java/ua/shortener/link/service/LinkService.java
@@ -2,6 +2,7 @@ package ua.shortener.link.service;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +30,8 @@ import java.util.HashMap;
 public final class LinkService {
 
     private final LinkRepository linkRepository;
+    @Getter
+    private final Map<String, Link> cache = new HashMap<>();
 
     public List<Link> getAllLinks() {
         return linkRepository.findAll();
@@ -39,6 +42,10 @@ public final class LinkService {
     }
 
     public void createLink(final Link link) {
+        String url = link.getUrl();
+        if (!url.startsWith("https://") || !url.startsWith("http://")){
+            link.setUrl("https://" + url);
+        }
         linkRepository.save(link);
     }
 
@@ -68,7 +75,7 @@ public final class LinkService {
         }
     }
 
-    public Link editLink(final Link existingLink) {
+    public Link editLink(Link existingLink) {
         if (linkRepository.findById(existingLink.getShortLink()).isEmpty()) {
             return null;
         }
@@ -108,12 +115,22 @@ public final class LinkService {
     }
 
     public void redirect(final String shortLink, final HttpServletResponse response) throws IOException {
-        Optional<Link> searchedLink = getLinkByShortLink(shortLink);
-        if (searchedLink.isPresent()) {
-            response.sendRedirect(searchedLink.get().getUrl());
-            Link link = searchedLink.get();
+        if (cache.containsKey(shortLink)){
+            Link cacheLink = cache.get(shortLink);
+            String url = cacheLink.getUrl();
+            response.sendRedirect(url);
+            cacheLink.setOpenCount(cacheLink.getOpenCount() + 1);
+            return;
+        }
+
+        Optional<Link> optionalSearchedLink = getLinkByShortLink(shortLink);
+        if (optionalSearchedLink.isPresent()) {
+            Link searchedLink = optionalSearchedLink.get();
+            response.sendRedirect(optionalSearchedLink.get().getUrl());
+            Link link = optionalSearchedLink.get();
             link.setOpenCount(link.getOpenCount() + 1);
             editLink(link);
+            cache.put(searchedLink.getShortLink(), searchedLink);
         } else {
             response.sendRedirect("/");
         }
--- src/main/java/ua/shortener/mvc/WebController.java
+++ src/main/java/ua/shortener/mvc/WebController.java
@@ -1,9 +1,9 @@
 package ua.shortener.mvc;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -26,6 +27,8 @@ import ua.shortener.security.auth.dto.registration.RegistrationRequest;
 import ua.shortener.user.User;
 import ua.shortener.user.service.UserService;
 
+import java.io.IOException;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -51,8 +54,8 @@ public final class WebController {
     }
 
     @PostMapping("/register")
-    public String registerUser(RegistrationRequest registrationRequest, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
+    public String registerUser(RegistrationRequest registrationRequest, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
 
             return "register";
         } else {
@@ -73,8 +76,12 @@ public final class WebController {
     }
 
     @PostMapping("/home_user/generate")
-    public String generateLink(final @RequestParam("url") String url, final RedirectAttributes redirectAttributes) throws ChangeSetPersister.NotFoundException {
-        User existingUser = userService.getUserById(1L)
+    public String generateLink(final @RequestParam("url") String url,
+                               final RedirectAttributes redirectAttributes,
+                               Principal principal) throws ChangeSetPersister.NotFoundException {
+        log.info("IN generateLink PRINCIPAL = " + principal.getName());
+
+        User existingUser = userService.findUserByEmail(principal.getName())
                 .orElseThrow(ChangeSetPersister.NotFoundException::new);
         Link link = new Link();
         link.setUrl(url);
@@ -92,9 +99,11 @@ public final class WebController {
     }
 
     @GetMapping("/home_user")
-    public ModelAndView showHomeUserPage() {
+    public ModelAndView showHomeUserPage(Principal principal) {
+        log.info("IN showHomeUserPage. PRINCIPAL = " + principal.getName());
+        User user = userService.findUserByEmail(principal.getName()).orElseThrow();
         ModelAndView result = new ModelAndView("/home_user");
-        List<Link> linkList = linkService.getAllLinks();
+        List<Link> linkList = user.getLinks();
         result.addObject("linkList", linkList);
         return result;
     }
@@ -108,14 +117,9 @@ public final class WebController {
     public ModelAndView showThanksToPage() {
         return new ModelAndView("/thanks_to");
     }
-}
 
-//if(parameterMap.keySet().contains("logout")){
-//        log.info("DO LOGOUT COOKIE WILL BE CHANGE TO DEPRECATE ONE");
-//        String oldToken = jwtTokenCookie.get().getValue();
-//        String invalidToken = jwtService.generateTokenInvalidToken(oldToken);
-//        Cookie cookie = new Cookie("jwtToken", invalidToken);
-//        response.addCookie(cookie);
-//        filterChain.doFilter(request, response);
-//        return;
-//        }
+    @GetMapping("/redirect/{shortLink}")
+    public void redirect(@PathVariable(name = "shortLink") String shortLink, HttpServletResponse response) throws IOException {
+        linkService.redirect(shortLink, response);
+    }
+}
