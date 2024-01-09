f98b381e69d1db67160c00b63571a05ffafee471
fear1611 <127629681+fear1611@users.noreply.github.com>
2024-01-09 16:35:20 +0200

    refactoring

--- src/main/java/ua/shortener/security/auth/dto/registration/RegistrationRequest.java
+++ src/main/java/ua/shortener/security/auth/dto/registration/RegistrationRequest.java
@@ -9,16 +9,9 @@ import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
-//@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RegistrationRequest {
 
-//    @NotBlank(message = "Username is required")
-//    @Size(min = 3, max = 16, message = "Username must be at least 3 characters long")
     String name;
-//    @NotBlank(message = "Password is required")
-//    @Size(min = 6, max = 24, message = "Password must be at least 6 characters long")
     String password;
-//    @Email(message = "Invalid email format")
-//    @NotBlank(message = "Email is required")
     String email;
 }
