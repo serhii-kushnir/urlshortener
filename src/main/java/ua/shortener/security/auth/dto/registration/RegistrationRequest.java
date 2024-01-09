9034da8b0a72a3e8c0025d9b7674edf695c3d424
fear1611 <127629681+fear1611@users.noreply.github.com>
2024-01-09 16:35:28 +0200

    refactoring

--- src/main/java/ua/shortener/security/auth/dto/registration/RegistrationRequest.java
+++ src/main/java/ua/shortener/security/auth/dto/registration/RegistrationRequest.java
@@ -1,12 +1,6 @@
 package ua.shortener.security.auth.dto.registration;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.experimental.FieldDefaults;
 
 @Data
 public class RegistrationRequest {
