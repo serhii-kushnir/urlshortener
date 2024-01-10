8237998f2e9e6ec827c5c5c09bab0fcdda17c176
Shurikosa <shurikosaula@gmail.com>
2024-01-10 20:03:10 +0200

    html zoom fix. register validation fix

--- src/main/java/ua/shortener/security/auth/dto/registration/RegistrationRequest.java
+++ src/main/java/ua/shortener/security/auth/dto/registration/RegistrationRequest.java
@@ -14,8 +14,8 @@ public class RegistrationRequest {
 
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 16, message = "Username must be at least 3 characters long")
-
     String name;@NotBlank(message = "Password is required")
+
     @Size(min = 6, max = 24, message = "Password must be at least 6 characters long")
     String password;@Email(message = "Invalid email format")
 
